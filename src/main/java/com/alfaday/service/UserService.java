e10b9fd1f88027d101c7de2e836e435ac4b2ae70
dailele <kunting007@163.com>
2021-01-22 16:40:59 +0800

    modify

--- src/main/java/com/alfaday/service/UserService.java
+++ src/main/java/com/alfaday/service/UserService.java
@@ -5,6 +5,7 @@ import com.alfaday.dao.mapper.UserDAO;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Data
 @Service
@@ -14,12 +15,18 @@ public class UserService {
     @Autowired
     private UserDAO userDAO;
 
+    @Transactional
     public String getUser(long id){
         UserDO one = userDAO.getOne(id);
         if(one != null){
             return one.getUsername();
         }
         return "no-user!";
+    }
+
+    @Transactional
+    public void insertUser(UserDO userDO){
+        userDAO.insert(userDO);
     }
 
 }
--- src/test/java/com/alfaday/application/SpringbootApplicationTests.java
+++ src/test/java/com/alfaday/application/SpringbootApplicationTests.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -24,6 +25,7 @@ public class SpringbootApplicationTests {
 	private UserDAO userDAO;
 
 	@Test
+	@Transactional
 	public void getUser() {
 		UserDO one = userDAO.selectById(1L);
 		logger.info("@@1@@  name=" + one.getUsername());
@@ -36,6 +38,7 @@ public class SpringbootApplicationTests {
 	}
 
 	@Test
+	@Transactional
 	public void insertUser(){
 		UserDO one = new UserDO();
 		one.setNickName("test");
@@ -44,6 +47,7 @@ public class SpringbootApplicationTests {
 		one.setUserSex("male");
 		int insert = userDAO.insert(one);
 		logger.info("insert ok, id={}",one.getId());
+
 	}
 
 }
