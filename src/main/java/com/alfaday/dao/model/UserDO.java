a6a1ff17b6d3ffc2da44091f75615e0394234f5b
dailele <kunting007@163.com>
2021-02-10 09:55:49 +0800

    modify

--- src/main/java/com/alfaday/dao/model/UserDO.java
+++ src/main/java/com/alfaday/dao/model/UserDO.java
@@ -1,10 +1,15 @@
 package com.alfaday.dao.model;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName(value = "user")
 public class UserDO {
 
+    @TableId(type = IdType.AUTO)
     private Long id;
     private String username;
     private String password;
